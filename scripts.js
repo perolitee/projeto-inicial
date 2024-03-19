bfb62c573633233431cf1ba47f97f1a7fe86d32c
perolitee <perolaozacarioto@gmail.com>
2024-03-19 08:23:22 -0300

    form

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
 function alertar(event){
-    alert("Você clicou no botão!! " + nome.value);
+    //alert("Você clicou no botão!! " + nome.value);
+
+    saida.innerText = "Nome: " +nome.value +
+                   "\n Email:: " + email.value;
 }
