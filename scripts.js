ddcc0b87fe1a0052b956f0a3ac77421990b41b1e
perolitee <perolaozacarioto@gmail.com>
2024-04-04 08:26:31 -0300

    organização saíde de dados

--- scripts.js
+++ scripts.js
@@ -9,6 +9,36 @@ var email = document.getElementById("email");
 function alertar(event){
     //alert("Você clicou no botão!! " + nome.value);
 
+const url = `https://viacep.com.br/ws/${cep.value}/json`;
+console.log(url)
+
+fetch(url).then(function(resposta){
+    return resposta.json()
+})
+.then(function(dados){
+    log.value = dados.logradouro;
+    ba.value = dados.bairro;
+    cid.value = dados.localidade;
+    est.value = dados.uf;
+
+    saidaDeDados();
+})
+.catch(function(error){
+    alert(error.message);
+})
+
+
+}
+
+function saidaDeDados(){
     saida.innerText = "Nome: " +nome.value +
-                   "\n Email:: " + email.value;
+    "\n Email: " + email.value +
+    "\n Telefone: " + tel.value +
+    "\n Cep: " + cep.value +
+    "\n Logradouro: " + log.value +
+    "\n Numero: " + num.value +
+    "\n Complemento: " + comp.value +
+    "\n Bairro: " + ba.value +
+    "\n Cidade: " + cid.value +
+    "\n Estado: " + est.value;
 }
